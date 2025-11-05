651d3fe3f1699662c996285b0f4af822147ee0cf
jamespencil <lockthomas065@gmail.com>
2025-11-05 10:27:27 +1000

    Add proper grammar.

--- src/bootl/stageTwo/stageTwo.c
+++ src/bootl/stageTwo/stageTwo.c
@@ -4,13 +4,13 @@
 // kernel.c
 void stageTwo() {
     vga_clear();
-    vga_print("Stage Two Loaded", 0, 0);
+    vga_print("Stage two loaded.", 0, 0);
     
     uint8_t devices[256];
     sata_scanAllPorts(devices);
     for (int i = 0; i < 256; i++) {
         if (devices[i] == 1)
-            vga_print("device found!", 0, 1);
+            vga_print("Device found!", 0, 1);
     }
     while(1);
 }
