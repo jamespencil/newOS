8880033114d13327add7fb389b126e9152fe094d
jamespencil <lockthomas065@gmail.com>
2025-11-05 10:13:49 +1000

    Fix vga.c broken newline logic
    typing \n would put the x at a random offset instead of resetting to 0, that has been fixed here.
    
    
    
    
    i know what you did on that linux pr callum.

--- src/bootl/stageTwo/drivers/vga.c
+++ src/bootl/stageTwo/drivers/vga.c
@@ -3,18 +3,30 @@
 void vga_clear() {
     volatile unsigned short *video = (unsigned short *)0xB8000;
     for (int i = 0; i < 80 * 25; i++) {
-        video[i] = 0x07 << 8 | ' ';  // attribute << 8 | char
+        video[i] = (0x07 << 8) | ' ';
     }
 }
 
 void vga_print(const char* str, int16_t x, uint8_t y) {
     volatile unsigned short *video = (unsigned short *)0xB8000;
+    int cx = x;
+    int cy = y;
+
     for (int i = 0; str[i]; i++) {
         if (str[i] == '\n') {
-            y++;
-            x= 0 - i - 1;
+            cy++;
+            cx = x;
             continue;
         }
-        video[y*80 + x + i] = (0x07 << 8) | str[i];  // attribute << 8 | char
+
+        if (cx >= 80) {
+            cx = 0;
+            cy++;
+        }
+        if (cy >= 25)
+            break;
+
+        video[cy * 80 + cx] = (0x07 << 8) | str[i];
+        cx++;
     }
 }
